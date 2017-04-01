ead9a6fe157937701a9d1b7a1011cc3c17e5ac26
BuildTools <unconfigured@null.spigotmc.org>
2017-04-01 05:58:27 +0200

    Update for AEON coin (cryptonight-light)

---
+++ website/config.js
@@ -0,0 +1,18 @@
+var api = "https://aeon.sumominer.com:9119";
+
+var poolHost = "aeon.sumominer.com";
+
+var irc = "irc.freenode.net/#aeon";
+
+var email = "support@sumominer.com";
+
+var cryptonatorWidget = ["AEON-BTC", "AEON-USD", "XMR-BTC", "ETH-BTC", "BTC-USD"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+
+var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+
+var themeCss = "themes/default-theme.css";
+//var themeCss = "themes/motherboard-dark-theme.css";
